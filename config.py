72b95282f65902d2e33405477cf335a9479b81cc
hxgzg6635jgd-art <hxgzg6635jgd@gmail.com>
2026-04-07 03:06:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    API_ID = int(environ.get("API_ID", "34599690"))
+    API_HASH = environ.get("API_HASH", "03dd1bece3ec246318f2a3ca5f0c1eac")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8015294485:AAF0no32M2CMKw8utr4SEZRhINMUldJYAyg") 
+    BOT_SESSION = environ.get("BOT_SESSION", "BQIP8woAnVXmZAt8I_ZqZVncZTS-Kbl312-obB3RJvO2E8a12RjMQJ3PEQGucalvNhLIXW26aE6hbfV74r0G5PsNDck8WgaTBIgC7CXUT1O-_n4WPuioD17g6HDEubdgfSuEPl3JUd3MIThar_4Fom_r_LnJ4JnQPswZF-wVA55A3g45dGV8sV1GcVdGUcscEx3oB-BTMO4UQ-BnVoFacP6oRMo8xJjILqtQ7auk651VNJpQqjNuOQ9hCn0EOPzFtMGuHQcfCXnS7LAoTUELpbDrXnbwMuyup_gmdCWMM0WjFnHsPGYOvbVpGVh3lu9pv1zQdLZ2r6po4gE7EyS_EJO6lG0x8wAAAAG8w1GTAA") 
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://hxgzg6635jg:kingking000@cluster0.wvtcac0.mongodb.net/?appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0)
+    BOT_OWNER = int(environ.get("BOT_OWNER", "7461884307"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
